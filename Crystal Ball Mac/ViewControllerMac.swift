c493eb0f659e6206dab9c84c95eef69cf0e0c1aa
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-09-01 13:50:52 +0800

    Created Mac UI
    Created empty IBActions
    Linked IBOutlets
    Started localization

---
+++ Crystal Ball Mac/ViewControllerMac.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Crystal Ball Mac
+//
+//  Created by Alessandro Vinciguerra on 18/08/2017.
+//  Copyright © 2017 Arc676. All rights reserved.
+//
+
+import Cocoa
+
+class ViewControllerMac : NSViewController {
+
+	@IBOutlet weak var questionField: NSTextField!
+	@IBOutlet var transcript: NSTextView!
+	
+	@IBAction func askQuestion(_ sender: Any) {
+	}
+
+	@IBAction func clearTranscript(_ sender: Any) {
+	}
+
+}
